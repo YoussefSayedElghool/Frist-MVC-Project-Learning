8eb894b5069af381943ff17bcea726b729be0b9a
Yossef Sayed Elgohl <Yossef Elgohl>
2023-11-16 04:05:34 +0200

    Add project files.

---
+++ Frist MVC Project Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frist_MVC_Project_Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
